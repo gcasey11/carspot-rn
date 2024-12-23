74b40aedccc3b91bcd8fca9f4a5f230ffe702905
Andrey Govorukhin <akgov74@gmail.com>
2024-12-23 14:23:42 +1100

    Removed sign up functionality from settings pages. Slight changes to text on settings and social feed pages.

--- screens/SocialScreen.tsx
+++ screens/SocialScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function SocialScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Social Feed Screen!</Text>
+      <Text style={styles.text}>Social Feed features Coming Soon!</Text>
     </View>
   );
 }
